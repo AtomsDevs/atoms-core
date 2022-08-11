2a4f9c83325bd0c529fd54793081edfd561b502a
mirkobrombin <send@mirko.pm>
2022-08-11 11:05:20 +0200

    add LICENSE

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1',
     packages=find_packages(),
     url='https://github.com/AtomsDevs/atoms-core',
-    license='MIT',
+    license='GPL-3.0-only',
     author='Mirko Brombin',
     author_email='send@mirko.pm',
     description='Atoms Core allows you to create and manage your own chroots'
