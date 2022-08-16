17863afc66b4059285ae2774390fed61ea229d18
mirkobrombin <send@mirko.pm>
2022-08-16 11:56:29 +0200

    gui support

--- atoms_core/wrappers/proot.py
+++ atoms_core/wrappers/proot.py
@@ -17,6 +17,7 @@
 import os
 import shutil
 import subprocess
+from pathlib import Path
 
 from atoms_core.utils.command import CommandUtils
 from atoms_core.exceptions.common import AtomsNoBinaryFound
@@ -36,6 +37,9 @@ class ProotWrapper:
         command: list = None,
         working_directory: str = None
     ) -> list:
+        if "DEV_BASH" in os.environ:
+            return CommandUtils.get_valid_command([("bash", "bin")])
+
         if command is None:
             command = []
 
@@ -44,11 +48,22 @@ class ProotWrapper:
 
         command = [
             ("env", "ext_bin"),
-            "-i", "HOME=/root", "HOSTNAME=atom", "TERM=xterm",
+            "-i", f"HOME={Path.home()}", "HOSTNAME=atom", "TERM=xterm", f"DISPLAY={os.environ['DISPLAY']}",
             self.__binary_path,
             "-w", working_directory,
-            # "-b", f"/run/user/{os.getuid()}",  # xorg experiments
-            "-S", chroot_path
+            "-0",
+            "-r", chroot_path,
+            "-b", "/etc/host.conf:/etc/host.conf",
+            "-b", "/etc/hosts:/etc/hosts",
+            "-b", "/etc/nsswitch.conf:/etc/nsswitch.conf",
+            "-b", "/etc/resolv.conf:/etc/resolv.conf",
+            "-b", "/dev:/dev",
+            "-b", "/sys:/sys",
+            "-b", "/proc:/proc",
+            "-b", "/tmp:/tmp",
+            "-b", f"{Path.home()}:{Path.home()}",
+            "-b", f"/run/user/{os.getuid()}:/run/user/{os.getuid()}",
+            "-b", "/usr/lib/x86_64-linux-gnu/GL/lib/dri/:/usr/lib/xorg/modules/dri",
         ] + command
-
+        print(" ".join(CommandUtils.get_valid_command(command)))
         return CommandUtils.get_valid_command(command)
