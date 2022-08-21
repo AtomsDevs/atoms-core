816f5faec67dc2bf92946d54f782991c6021f1bb
mirkobrombin <send@mirko.pm>
2022-08-21 15:09:04 +0200

    update ubuntu remote

--- atoms_core/entities/distributions/ubuntu.py
+++ atoms_core/entities/distributions/ubuntu.py
@@ -9,32 +9,17 @@ class Ubuntu(AtomDistribution):
             distribution_id="ubuntu",
             name="Ubuntu",
             logo="ubuntu-symbolic",
-            releases=["jammy-20220818_07:42", "1kinetic-20220818_07:43"],
-            remote_structure="https://uk.lxd.images.canonical.com/images/ubuntu/{0}/{1}/default/{2}/rootfs.tar.xz",
-            remote_hash_structure="https://uk.lxd.images.canonical.com/images/ubuntu/{0}/{1}/default/{2}/SHA256SUMS",
+            releases=["22.04", "20.04"],
+            remote_structure="http://cdimage.ubuntu.com/ubuntu-base/releases/{0}/release/ubuntu-base-{0}-base-{1}.tar.gz",
+            remote_hash_structure="http://cdimage.ubuntu.com/ubuntu-base/releases/{0}/release/SHA256SUMS",
             remote_hash_type="sha256",
             architectures={"x86_64": "amd64"},
             root="",
-            container_image_name="ubuntu",
+            container_image_name="ubuntu"
         )
 
-    def get_remote(self, architecture: str, release: str) -> str:
-        # split release into version and build date
-        release, build_date = release.split('-')
-        return self.remote_structure.format(
-            release,
-            architecture,
-            build_date
-        )
-
-    def get_remote_hash(self, architecture: str, release: str) -> str:
-        # split release into version and build date
-        release, build_date = release.split('-')
-        return self.remote_hash_structure.format(
-            release,
-            architecture,
-            build_date
-        )
+    def get_remote_hash(self, _, release: str) -> str:
+        return self.remote_hash_structure.format(release)
 
     def post_unpack(self, chroot):
         # workaround Code:APT_UNTRUSTED_KEYS
