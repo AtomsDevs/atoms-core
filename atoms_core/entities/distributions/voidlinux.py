6b61433d24085c4751e21829f028d4432d95f6d1
mirkobrombin <send@mirko.pm>
2022-08-22 22:23:50 +0200

    use lxd repo for voidlinux

--- atoms_core/entities/distributions/voidlinux.py
+++ atoms_core/entities/distributions/voidlinux.py
@@ -7,11 +7,24 @@ class VoidLinux(AtomDistribution):
             distribution_id="voidlinux",
             name="Void Linux",
             logo="void-linux-symbolic",
-            releases=["20210930", ],
-            remote_structure="https://repo-default.voidlinux.org/live/{0}/void-{1}-ROOTFS-{0}.tar.xz",
-            remote_hash_structure=None,# TODO: support custom struct: https://repo-default.voidlinux.org/live/20210930/sha256sum.txt
-            remote_hash_type=None,
-            architectures={"x86_64": "x86_64"},
+            releases=["current", ],
+            remote_structure=None,
+            remote_hash_structure=None,
+            remote_hash_type="sha256",
+            architectures={"x86_64": "amd64"},
             root="",
             container_image_name="voidlinux",
         )
+
+    def __get_base_path(self, architecture: str, release: str) -> str:
+        base_url = "https://uk.lxd.images.canonical.com/images/voidlinux/{release}/{architecture}/default".format(
+            release=release, architecture=architecture
+        )
+        build = self._get_latest_remote_dir(base_url)
+        return "{0}/{1}".format(base_url, build)
+        
+    def get_remote(self, architecture: str, release: str) -> str:
+        return "{0}/rootfs.tar.xz".format(self.__get_base_path(architecture, release))
+            
+    def get_remote_hash(self, architecture: str, release: str) -> str:
+        return "{0}/SHA256SUMS".format(self.__get_base_path(architecture, release))
