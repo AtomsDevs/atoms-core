49a0b1d21099af61acc4875c500804f7da9243f1
mirkobrombin <send@mirko.pm>
2022-08-24 10:13:35 +0200

    update NO_APT_CHWN_PERM workaround

--- atoms_core/entities/distributions/ubuntu.py
+++ atoms_core/entities/distributions/ubuntu.py
@@ -57,6 +57,11 @@ Good luck!
         with open(os.path.join(chroot, "etc/apt/apt.conf.d/01atom"), "w") as f:
             f.write("APT::Sandbox \"0\";")
             f.write("APT::Sandbox::User \"root\";")
+        with open(os.path.join(chroot, "etc/passwd"), "r") as f:
+            data = f.read()
+            data = data.replace("_apt:x", "#_apt:x")
+            with open(os.path.join(chroot, "etc/passwd"), "w") as f:
+                f.write(data)
         
         # workaround Code:PATH_NOT_DEFINED
         with open(os.path.join(chroot, "etc/profile.d/atom.sh"), "w") as f:
